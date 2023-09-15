2029f7369f41d378881a4000cb5482d18cc7ba0c
ilimic1 <ilimic1@gmail.com>
2023-09-15 10:55:18 +0000

    Update from https://github.com/ilimic1/php-monorepo/commit/2563a0e89c4d8ddbe9a2866a15f467a03e74e55a

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ilimic/hello-world-3',
+        'pretty_version' => 'v1.0.16',
+        'version' => '1.0.16.0',
+        'reference' => '2563a0e89c4d8ddbe9a2866a15f467a03e74e55a',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ilimic/hello-world-3' => array(
+            'pretty_version' => 'v1.0.16',
+            'version' => '1.0.16.0',
+            'reference' => '2563a0e89c4d8ddbe9a2866a15f467a03e74e55a',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v5.2',
+            'version' => '5.2.0.0',
+            'reference' => '5a270988c5f76bfdfbbb42cccc7c9627f7dd64d0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
